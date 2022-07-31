568330ecc8e0114392cbdac002670e62d10d79ae
Christopher <christansbur@gmail.com>
2022-07-31 18:19:53 -0500

    add challenge param http response for slack event subscription

--- Beatbot/Function1.cs
+++ Beatbot/Function1.cs
@@ -18,18 +18,18 @@ namespace Beatbot
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            string challenge = data?.challenge;
+            Response response = new Response();
+            response.challenge = challenge;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(response);
         }
+    }
+
+    public class Response
+    {
+        public string challenge { get; set; } = "";
     }
 }
